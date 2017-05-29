54b3c3e00039f500ca0448086cbfadb772113620
Osmar Silva <osmar.silva@sythex.com.br>
2017-05-29 17:35:53 -0300

    Commit parcial JavaScript Avançado II

--- aluraframe/client/js/controllers/NegociacaoController.js
+++ aluraframe/client/js/controllers/NegociacaoController.js
@@ -40,4 +40,16 @@ class NegociacaoController {
         this._inputData.focus();
     }
 
+    importaNegociacoes(){
+        let service = new NegociacaoService();
+        service.obterNegociacoesDaSemana((erro, negociacoes) => {
+            if(erro){
+                this._mensagem.texto = erro;
+                return;
+            }
+            negociacoes.forEach(negociacao => this._listaNegociacoes.adiciona(negociacao));
+            this._mensagem.texto = 'Negociacoes importadas com sucesso!';
+        });
+    }
+
 }
